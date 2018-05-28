0becc13ac578ceea39b779cb0d18f177e6eb9a3f
captain-porkins <33499113+captain-porkins@users.noreply.github.com>
2018-05-28 23:02:06 +0100

    added dipole field

---
+++ galactic/fields/dipole.py
@@ -0,0 +1,31 @@
+from scipy.constants import mu_0, epsilon_0, pi
+import numpy as np
+
+
+class Dipole(object):
+    def __init__(self, moment_vector=None, moment_magnitude=None, moment_orientation=None, charge=None, mass=None):
+        if moment_vector is not None:
+            if moment_orientation is not None:
+                raise ValueError('Do not specify both a moment vector and a moment orientation')
+            else:
+                self.moment_vector = moment_vector
+        elif moment_magnitude is not None:
+            moment_orientation = moment_orientation or (0, 0, 1)
+            self.moment_vector = np.multiply(moment_magnitude, moment_orientation)
+
+        if charge is not None:
+            raise NotImplementedError('Charged dipoles not yet implemented')
+
+        if mass is not None:
+            raise NotImplementedError('Massive dipoles not yet implemented')
+
+    def magnetic_field(self, position):
+        scalar_position = np.linalg.norm(position)
+        prefactor = (mu_0 / (4 * pi * scalar_position ** 3))
+        first_term = np.multiply(3 * np.dot(self.moment_vector, position) / (scalar_position**2), position)
+        return prefactor * np.subtract(first_term, self.moment_vector)
+
+
+if __name__ == '__main__':
+    dipole = Dipole(moment_magnitude=10)
+    b = dipole.magnetic_field(np.matrix([[10], [1], [4.7]]))
